498e7229fe8a54fe729777ba20c4fafe1ac82e98
Alisson Assis <ninjalendario012@gmial.com>
2024-04-27 19:11:29 -0300

    [Created] section galery img

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,34 @@ var swiper = new Swiper(".equipe", {
     900: {
       slidesPerView: 4,
       spaceBetween: 26,
+      centeredSlides: false,
+    },
+  },
+});
+
+var swiper = new Swiper(".galeria", {
+  slidesPerView: 1,
+  spaceBetween: 1,
+  centeredSlides: true,
+  loop: true,
+  effect: "slide",
+  autoplay: {
+    delay: 5000,
+  },
+  navigation: {
+    nextEl: ".btn-next-galeria",
+    prevEl: ".btn-prev-galeria",
+  },
+  breakpoints: {
+    400: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+        centeredSlides: false,
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+      centeredSlides: false,
     },
   },
 });
