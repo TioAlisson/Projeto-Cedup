ae914c2a00861e401778db4cfa030c52c50134e0
Alisson Assis <ninjalendario012@gmial.com>
2024-05-01 18:59:45 -0300

    [Fix] menu, slide, ands cards carrousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,3 +51,31 @@ var swiper = new Swiper(".galeria", {
     },
   },
 });
+
+Fancybox.bind("[data-fancybox]", {
+});
+
+// NAVBAR FIXED > 10
+//use window.scrollY
+let scrollpos = window.scrollY;
+let header = document.getElementById("menu");
+
+function add_class_on_scroll() {
+    header.classList.add("compressed");
+}
+
+function remove_class_on_scroll() {
+    header.classList.remove("compressed");
+}
+
+window.addEventListener('scroll', function(){ 
+    //Here you forgot to update the value
+    scrollpos = window.scrollY;
+
+    if(scrollpos > 10){
+        add_class_on_scroll();
+    }
+    else {
+        remove_class_on_scroll();
+    }
+});
